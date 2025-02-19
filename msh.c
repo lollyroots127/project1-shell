f3d5e5d62629ecec238aef52e1846a2da0bc6020
Laura Roots <laura.roots.99@gmail.com>
2025-02-19 14:15:11 -0600

    Finish implementation and clean comments

--- msh.c
+++ msh.c
@@ -1,25 +1,8 @@
-// The MIT License (MIT)
-// 
-// Copyright (c) 2016 Trevor Bakker 
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+/*
+
+  Name: Laura Roots
+  ID:   1001637158
+*/
 
 #define _GNU_SOURCE
 
@@ -38,7 +21,7 @@
 
 #define MAX_COMMAND_SIZE 255    // The maximum command-line size
 
-#define MAX_NUM_ARGUMENTS 11     // Mav shell only supports ten arguments
+#define MAX_NUM_ARGUMENTS 11    // Mav shell only supports ten arguments
 
 #define MAX_HISTORY_SIZE 15     // The maximum commands stored in history
 
@@ -47,9 +30,11 @@
 int main()
 {
 
-  char *command_string = (char*)malloc(MAX_COMMAND_SIZE);
-  char *history[MAX_HISTORY_SIZE] = {NULL};
-  pid_t pid_history[MAX_PID_HISTORY_SIZE] = {};
+  char *command_string = (char*)malloc(MAX_COMMAND_SIZE); // Dynamically allocate memory 
+                                                          // to store user commands
+  char *history[MAX_HISTORY_SIZE] = {NULL};               // Store the max number of commands
+  pid_t pid_history[MAX_PID_HISTORY_SIZE] = {};           // Store max number of spawned child 
+                                                          // processes
   int history_count = 0, pid_count = 0;
 
   while (1) {
@@ -63,19 +48,39 @@ int main()
     // is no input
     while (!fgets(command_string, MAX_COMMAND_SIZE, stdin));
 
-    // Store command in history
+    // If no command entered, repeat loop until proper input is detected
+    if (command_string[0] == '\n') continue;
+
+    // Store the current command in history
+    // Do this before evaluating commands to ensure they are stored, including invalid commands
+    // Empty commands are not stored
     if (history_count < 15) {
       history[history_count] = strdup(command_string);
       history_count++;
-    } else {
-      free(history[0]);
+    } else { 
+      // If maximum history is already reached, free oldest command and shift array to only
+      // show the most recent commands
+      free(history[0]); 
       for (int i = 1; i < MAX_HISTORY_SIZE; i++) {
         history[i - 1] = history[i];
       }
       history[MAX_HISTORY_SIZE - 1] = strdup(command_string);
     }
-    
 
+    // If the user enters the "!n" command, verify that it is an available command and then
+    // Overwrite the command string and continue with execution.
+    // Note: since the "!n" command is stored in history before execution, 
+    // it will be the most recent command
+    if (command_string[0] == '!') {
+      int n = atoi(&command_string[1]);
+      if (n < 0 || n >= history_count) {
+        printf("Command not in history.\n");
+        continue;
+      } else {
+        strcpy(command_string, history[n]);
+      }
+    }
+    
     /* Parse input */
     char *token[MAX_NUM_ARGUMENTS];
 
@@ -102,15 +107,6 @@ int main()
         token_count++;
     }
 
-    // Print the tokenized input as a debug check
-    int token_index  = 0;
-    for (token_index = 0; token_index < token_count; token_index++) {
-      printf("token[%d] = %s\n", token_index, token[token_index]);
-    }
-
-    // If no command entered, repeat loop until proper input is detected
-    if (token[0] == NULL) continue;
-
     // Check if command is to exit or quit, then free memory and exit
     if (strcmp(token[0], "exit") == 0 || strcmp(token[0], "quit") == 0) {
       printf("Exiting msh...\n");
@@ -121,44 +117,69 @@ int main()
       exit(0);
     }
 
-    // TODO: Implement "cd" command
+    // Implement the "cd" command in the parent process using chdir because it affects the 
+    // current working directory and would be lost in a child process once it is terminated.
     if (strcmp(token[0], "cd") == 0) {
-      if (token[1] == NULL) {
+      if (token[1] == NULL) { // Verify that a location was provided
           printf("cd: no directory provided\n");
       } 
       else {
-          if (chdir(token[1]) != 0) {
-              perror("cd failed");
-          }
+        // token[1] contains the location and if ".." was entered, chdir is able to change directory
+        // to the parent directory of our current process
+        if (chdir(token[1]) != 0) {
+            perror("cd failed");
+        }
+      }
+      continue;
+    }
+        
+    // Implement "history" command to display the last commands entered, up to history_count, which
+    // will not surpass 15.
+    if (strcmp(token[0], "history") == 0) {
+      for (int i = 0; i < history_count; i++) {
+        printf("%d: %s", i, history[i]);
       }
       continue;
     }
 
-    // TODO: Implement "pidhistory" command to display last 15 process IDs
-        
-    // TODO: Implement "history" command to display last 15 user commands
-        
-    // TODO: Implement "!n" to execute nth command from history
-
-    // TODO: Implement command execution
-    pid_t pid = fork( );
-    if( pid == 0 )
-    {
-      int ret = execvp( token[0], &token[0] );  
-      if( ret == -1 )
-      {
-        perror( "execvp failed: " );
+    // Implement "showpids" command to display last 15 process IDs. These are created when a child
+    // process is created and does not include the current (parent) process.
+    if (strcmp(token[0], "showpids") == 0) {
+      for (int i = 0; i < pid_count; i++) {
+        printf("%d: %d\n", i, pid_history[i]);
       }
-    }
-    else 
-    {
-      int status;
-      wait( & status );
+      continue;
     }
 
-    free( head_ptr );
+    // Execute external commands through the child process.
+    pid_t pid = fork();
+    if (pid == 0) {
+      // If pid is 0, then we are in the child process and can execute the command
+      int ret = execvp(token[0], &token[0]);  
+      if (ret == -1) {
+        printf("%s: Command not found.\n", token[0]);
+      }
+    } else if (pid == -1) {
+      perror("fork failed");
+    } else {
+      // In the parent process, store the child pid and wait until the child process finishes
+      // execution of the command.
+      // If max pid history is reached, then shift pid history over to include most recent command.
+      if (pid_count < 15) {
+        pid_history[pid_count] = pid;
+        pid_count++;
+      } else {
+        for (int i = 1; i < MAX_PID_HISTORY_SIZE; i++) {
+          pid_history[i-1] = pid_history[i];
+        }
+        pid_history[MAX_HISTORY_SIZE - 1] = pid;
+      }
+      int status;
+      wait(&status);
+    }
+
+    free(head_ptr); // Free allocated memory.
 
   }
   return 0;
-  // e2520ca2-76f3-90d6-0242ac120003
 }
